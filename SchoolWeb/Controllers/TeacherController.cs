a30faf0642c8adfbcddec8905a176906c36eb016
nab <nab@example.com>
2022-04-07 15:34:27 +0200

    teacher

--- SchoolWeb/Controllers/TeacherController.cs
+++ SchoolWeb/Controllers/TeacherController.cs
@@ -1,9 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SchoolWeb.Models;
 
 namespace SchoolWeb.Controllers
 {
     public class TeacherController : Controller
     {
+        private static List<Teacher>? teacher;
+
+        public TeacherController()
+        {
+            if (teacher == null || !teacher.Any())
+            {
+                teacher = new List<Teacher>()
+                {
+                    new Teacher()
+                    {
+                        PersonId = 1,
+                        FirstName = "Ted",
+                        LastName = "Mosby",
+                        Age = 35,
+                        Discipline = "Architect",
+                        HiringDate = new DateTime(2008, 12, 30)
+                    },
+                    new Teacher()
+                    {
+                        PersonId = 2,
+                        FirstName = "Barney",
+                        LastName = "Stinson",
+                        Age = 38,
+                        Discipline = "Economy",
+                        HiringDate = new DateTime(2020, 01, 25)
+                    },
+                    new Teacher()
+                    {
+                        PersonId = 3,
+                        FirstName = "Lily",
+                        LastName = "Aldrin",
+                        Age = 36,
+                        Discipline = "Generalist",
+                        HiringDate = new DateTime(2016, 02, 21)
+                    }
+                };
+            }
+        }
+
         // CRUD
 
         public IActionResult Index()
